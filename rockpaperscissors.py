8626f34c9eca6cb2991397ec536bd6b0f52778a5
theTheodroid14 <theoalex.bman@gmail.com>
2025-01-17 19:25:00 +0700

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,41 @@
+import random
+
+ourmove = input("Rock, Paper or Scissors? ").lower()
+
+
+opponentsmoveint = random.randint(1, 3)
+
+def pick():
+    global opponentsmove  
+    if opponentsmoveint == 1:
+        opponentsmove = "rock"
+    elif opponentsmoveint == 2:
+        opponentsmove = "paper"
+    else:
+        opponentsmove = "scissors"
+
+pick()
+
+print("Opponent chose: " + opponentsmove)
+
+def check():
+    
+    if ourmove not in ["rock", "paper", "scissors"]:
+        print("Answer correctly (choose rock, paper, or scissors)")
+        return  
+    
+    # Check the answer for win/lose/tie outcome
+    if opponentsmove == "rock" and ourmove == "scissors":
+        print("You Lose!")
+    elif opponentsmove == "scissors" and ourmove == "paper":
+        print("You Lose!")
+    elif opponentsmove == "paper" and ourmove == "rock":
+        print("You Lose!")
+    elif opponentsmove == ourmove:  
+        print("It's a tie!")
+    else:
+        print("You win!")
+
+check()
+
+input("Game is finished Press Enter to close!")
